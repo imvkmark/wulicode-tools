16a687e89713ae4dc7d205cfa651f22c405a28dc
多厘 <zhaody901@126.com>
2022-02-25 09:42:06 +0800

    task # 添加缓存清理

--- src/components/base/PxTheme.vue
+++ src/components/base/PxTheme.vue
@@ -3,13 +3,16 @@
         <MagicStick/>
     </ElIcon>
     <ElDrawer v-model="trans.visible" title="主题设定">
-        <ElForm>
-            <ElFormItem label="元素大小" :size="trans.elementSize">
+        <ElForm :size="trans.elementSize">
+            <ElFormItem label="元素大小">
                 <ElRadioGroup :model-value="trans.elementSize" @update:model-value="onUpdateElementSize">
                     <ElRadioButton label="small">小号</ElRadioButton>
                     <ElRadioButton label="default">默认</ElRadioButton>
                     <ElRadioButton label="large">大号</ElRadioButton>
                 </ElRadioGroup>
+            </ElFormItem>
+            <ElFormItem label="清理缓存" :size="trans.elementSize">
+                <ElButton :loading="trans.clearing" @click="onClearCache">清理</ElButton>
             </ElFormItem>
         </ElForm>
     </ElDrawer>
@@ -20,6 +23,9 @@ import { computed, reactive } from 'vue';
 import { useRouter } from 'vue-router';
 import { useStore } from '@/store';
 import { MagicStick } from "@element-plus/icons-vue";
+import { pyStorageKey } from "@/framework/utils/conf";
+import { localStore, toast } from "@/framework/utils/helper";
+import { apiMgrAppHomeClearCache } from "@/framework/services/mgr-app";
 
 // 监听路由前缀的变化
 let router = useRouter();
@@ -27,6 +33,7 @@ let store = useStore();
 const trans = reactive({
     elementSize: computed(() => store.state.poppy.elementSize),
     visible: false,
+    clearing: false,
 });
 
 const onUpdateElementSize = (value: string) => {
@@ -34,6 +41,18 @@ const onUpdateElementSize = (value: string) => {
 }
 const onSwitchDrawer = () => {
     trans.visible = !trans.visible;
+}
+
+const onClearCache = () => {
+    trans.clearing = true;
+    localStore(pyStorageKey.navs, null);
+    apiMgrAppHomeClearCache().then(() => {
+        toast('已清空缓存, 稍后会进行页面刷新');
+        trans.clearing = false;
+        setTimeout(() => {
+            store.commit('poppy/SET_ACTION', 'reload');
+        }, 1000);
+    })
 }
 
 </script>
