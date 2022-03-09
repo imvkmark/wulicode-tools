4a5a5547f6bba509c4ff086236adfba2aca4b88d
多厘 <zhaody901@126.com>
2022-03-09 16:10:34 +0800

    test: 加入 simple_pwd 规则 close#3

--- test/helper.test.ts
+++ test/helper.test.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 import assert from 'assert';
-import { regexTest, sprintf } from '@/framework/utils/helper';
+import { isSimplePwd, pyWarning, regexTest, sprintf } from '@/framework/utils/helper';
 
 describe('regex', function () {
     it('test-number', function () {
@@ -23,5 +23,16 @@ describe('string', function () {
         it('replace 2 param', function () {
             assert.equal('你是什么', sprintf('{0}是什{1}', '你', '么'));
         });
+    });
+});
+
+
+describe('project', function () {
+    it('is-simple_pwd', function () {
+        assert.equal(isSimplePwd('12345'), false);
+        assert.equal(isSimplePwd('12345678901234567'), false);
+        assert.equal(isSimplePwd('1zA*_.[]-!@$%^'), true);
+        assert.equal(isSimplePwd('-!@#$%^&()~Nk03s'), true);
+        assert.equal(isSimplePwd(' 1255NKU'), false);
     });
 });
